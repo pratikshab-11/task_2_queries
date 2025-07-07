0eb2799aaf4523f2c3fd86d19f3e04a327cb131f
pratikshab-11 <pratikshabagde99@gmail.com>
2025-07-08 01:06:46 +0530

    Add files via upload

---
+++ query _file_SQL_task - Copy.sql
@@ -0,0 +1,104 @@
+1 st querry
+
+select * from board 
+select * from board where marks >450
+
+select * from board
+
+update board set schl_name =4567 where marks = 458
+
+select * from board where marks <=450
+
+select * from board
+
+delete from board 
+select * from board
+rollback
+
+select * from board
+
+select * from board
+
+2query 
+
+
+
+select * from brands
+
+select * from brands where sale >=20000
+
+select * from brands where sale >=20600
+
+select * from brands 
+
+update brands set name =1990 where sale = 20001
+
+select * from brands 
+
+delete from brands
+
+select * from brands
+rollback
+
+select * from brands
+
+
+3query
+
+select * from insurance
+
+select * from insurance where ncb=35
+
+select * from insurance
+
+update insurance set mng =678 where ncb=35
+
+delete from insurance
+
+select * from insurance
+
+rollback
+
+select * from insurance
+
+
+   
+query 4
+
+select * from popul
+
+select * from popul where age>=27
+
+
+update popul set name=5787 where age =29
+
+select * from popul
+
+delete from popul
+
+select * from popul
+
+rollback
+
+select * from popul
+
+query 5
+select * from veg
+
+
+select * from veg where production =1374
+
+select * from veg
+
+update veg set cities=4566 where kg=223
+
+select * from veg
+
+delete from veg
+
+select * from veg
+
+rollback
+
+select * from veg
+
